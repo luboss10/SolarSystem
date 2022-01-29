1471f320b423044b2813fcfb46fb9b69d795504c
Lubo <luboss10@gmail.com>
2022-01-29 11:04:07 -0500

    the basic structure is complete

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,10 @@ public class Main {
     public static void main(String[] args) {
 
         solarSystem milkyway=new solarSystem();
-        
+        milkyway.sun=1;
+        milkyway.moon=2;
+        milkyway.planets=3;
+
+
     }
 }
